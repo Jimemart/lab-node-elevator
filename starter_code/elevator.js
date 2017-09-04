64ca83a57100759d24037cdc343d78d560342286
Jimena Martin <jimena.mg@hotmail.com>
2017-09-04 17:24:13 +0200

    It's working, needs clearing

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,18 +1,54 @@
+/*jshint esversion: 6 */
 class Elevator {
   constructor(){
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction = "up";
+    this.waitingList = [];
+    this.passengers = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
+  start() {this.interval = setInterval(()=>this.update(),1000)}
+  stop() {if(this.requests.length)setTimeout(()=>clearInterval(this.interval),3000)}
+
+  update() {
+
+    this.floorUp();
+    console.log(`The elevator is at ${this.floor} going ${this.direction}`);
+    var that = this;
+    this.waitingList.forEach(function(value,index){
+      if(that.floor === value.originFloor){
+        that._passengersEnter(value,index);
+        console.log(`${value.name} has entered the elevator`);
+      }
+    });
+    console.log(this.passengers);
+    this.passengers.forEach(function(value,index){
+      if(that.floor == value.destinationFloor){
+        that._passengersLeave(value,index);
+        console.log(`${value.name} has left the elevator`)
+      }
+    });
+  }
+
+  _passengersEnter(value,index) {
+    this.passengers= [...this.passengers,...this.waitingList.splice(index,1)];
+    this.requests.splice(index,1);
+    this.requests.push(value.destinationFloor);
+  }
+  _passengersLeave(value,index) {
+    this.passengers.splice(index,1);
+    this.requests.splice(index,1);
+  }
+
+
+  floorUp() {if(this.floor < this.MAXFLOOR) this.floor +=1;}
+  floorDown() {if(this.floor > 0)this.floor -=1;}
+  call(person) {
+    this.requests.push(person.originFloor);
+    this.waitingList.push(person);
+  }
   log() { }
 }
 
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,8 @@
+/*jshint esversion: 6 */
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+let elevator = new Elevator();
+const person1 = new Person("Jimena", 2, 5);
+
+elevator.call(person1);
+elevator.start();
